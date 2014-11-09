6bbc60124e5f6a9c6999c7538e60735df5ab12aa
Ahmed Khedr <ahmed.a.khedr@outlook.com>
2014-11-08 21:52:50 -0400

    Refactor part1

--- part1.rb
+++ part1.rb
@@ -1,33 +1,11 @@
 def sum(x)
-	total = 0
-	x.each do |elem|
-		total += elem
-	end
-	total
+	x.inject(0, :+)
 end
 
 def max_2_sum(x)
-	if x.length == 0
-		0
-	elsif x.length == 1
-		x[0]
-	else
-		x.sort!
-		x[-1] + x[-2]
-	end
+	x.empty? ? 0 : x.sort![-1] + (x[-2] or 0)
 end
 
 def sum_to_n?(x, n)
-	if x.length == 0 || x.length == 1
-		return false
-	else
-		x.each do |x1|
-			x.each do |x2|
-				if x1 != x2 && x1 + x2 == n
-					return true
-				end
-			end
-		end
-	end
-	return false
+	x.combination(2).any? { |x1, x2| x1 + x2 == n }
 end
